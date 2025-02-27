7c7973388badebb5606f76261a215d1b8713e339
Sinh Kavya <kavya.singh@shunyavkash.com>
2025-02-27 17:54:00 +0530

    home-pages completed

--- assests/js/main.js
+++ assests/js/main.js
@@ -69,10 +69,10 @@ $(".slider-mainbox").slick({
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
+          slidesToShow: 3,
+          slidesToScroll: 2,
           infinite: true,
-          dots: true,
+          dots: false,
         },
       },
       {
