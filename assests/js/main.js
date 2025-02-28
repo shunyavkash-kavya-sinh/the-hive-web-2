2466ae531a7cc0df7f4200bcfd7c965e06d0433a
Sinh Kavya <kavya.singh@shunyavkash.com>
2025-02-28 13:02:59 +0530

    we-are

--- assests/js/main.js
+++ assests/js/main.js
@@ -93,3 +93,13 @@ $(".slider-mainbox").slick({
   });
 
 // resources end
+
+// circle-data start
+$(".circle-autoplay").slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows:false,
+  autoplaySpeed: 1000,
+});
+// circle-data end
